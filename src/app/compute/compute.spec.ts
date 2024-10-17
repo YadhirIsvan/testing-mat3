e9b68e013fce3ff71fd2b52ed8d4d9ac4ba3511a
YadhirIsvan <yadhirisvan11@gmail.com>
2024-10-17 00:04:19 -0600

    add compute branch #1

---
+++ src/app/compute/compute.spec.ts
@@ -0,0 +1,16 @@
+import { compute } from './compute';
+
+describe('compute test suite', () => {
+
+  it('Should return 0 if input is negative', () => {
+    const result = compute(-1);
+    expect(result).toBe(0);
+  });
+
+  it('Should increment the input if it is positive', () => {
+    const result = compute(1);
+    expect(result).toBe(2);  // Corrige el valor esperado si es necesario
+  });
+
+});
+
